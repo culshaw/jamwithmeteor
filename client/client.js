c9881778c7f924c294b87cc6b7fb0b78b9d4b413
culshaw <culshaw@gmail.com>
2013-08-15 17:45:46 +0100

    Piano pitch

--- client/client.js
+++ client/client.js
@@ -9,14 +9,13 @@
 
 
     Deps.autorun(function () {
-        Meteor.subscribe("locations");        
+        Meteor.subscribe("locations");
     });
 
     Meteor.call('takeSample', {}, function(err, result) {
         if(!err)
-            console.log(result);
 
-            if(result.id != 0) {
+            if(result.id !== 0) {
                 Session.set('sample', result.id);
 
                 bufferLoader = new BufferLoader(
@@ -32,11 +31,59 @@
                 bufferLoader.load();
             } else {
                 // Render correct HTML?
-                $('body').append(Template.instrumentGuitar());
+                //$('body').append(Template.instrumentGuitar());
 
-                oscillator = new OscilatorCreator(context);
+                
+                // Create synth oscillator tone, whatever you want to call it.
+                //oscillator = new OscilatorCreator(context);
+                var nodes  = document.querySelectorAll('li'),
+                    _nodes = [].slice.call(nodes, 0);
+
+                //parseNodes(_nodes);
+
+                document.ontouchmove = function(event){
+                    event.preventDefault();
+                };
+
+                var getDirection = function (ev, obj) {
+                    var w = obj.offsetWidth,
+                        h = obj.offsetHeight,
+                        x = (ev.pageX - obj.offsetLeft - (w / 2) * (w > h ? (h / w) : 1)),
+                        y = (ev.pageY - obj.offsetTop - (h / 2) * (h > w ? (w / h) : 1)),
+                        d = Math.round( Math.atan2(y, x) / 1.57079633 + 5 ) % 4;
+
+                    return d;
+                };
+
+                var addClass = function ( ev, obj, state ) {
+                    var direction = getDirection( ev, obj ),
+                        class_suffix = "";
+
+                    obj.className = "";
+
+                    switch ( direction ) {
+                        case 0 : class_suffix = '-top';    break;
+                        case 1 : class_suffix = '-right';  break;
+                        case 2 : class_suffix = '-bottom'; break;
+                        case 3 : class_suffix = '-left';   break;
+                    }
+
+                    obj.classList.add( state + class_suffix );
+                };
+
+                // bind events
+                _nodes.forEach(function (el) {
+                    el.addEventListener('mouseover', function (ev) {
+                        addClass( ev, this, 'in' );
+                    }, false);
+
+                    el.addEventListener('mouseout', function (ev) {
+                        addClass( ev, this, 'out' );
+                    }, false);
+                });
+
             }
-    })
+    });
 
     function OscilatorCreator(context) {
         this.oscillator = context.createOscillator(); // Create bass guitar
@@ -48,7 +95,7 @@
         this.gainNode.connect(this.context.destination); // Connect boost pedal to amplifier
 
         this.oscillator.connect(this.biQuad);
-        this.biQuad.connect(this.context.destination)
+        this.biQuad.connect(this.context.destination);
 
         this.biQuad.type = 3;
         this.biQuad.frequency.value = 120;
@@ -56,7 +103,7 @@
         this.biQuad.gain.value = 0;
 
         this.gainNode.gain.value = 0.3; // Set boost pedal to 30 percent volume
-        this.oscillator.noteOn(0); // Play bass guitar instantly
+        //this.oscillator.noteOn(0); // Play bass guitar instantly
 
         node = this;
         document.getElementById('oscillator').addEventListener('touchmove', function(ev) { node.onInteract(ev, node); });
@@ -71,7 +118,7 @@
 
         //oscillator.type = parseInt(touch0); // Square wave
         //console.log(node.biQuad.frequency.value);
-        node.oscillator.frequency.value = parseInt(touch0)
+        node.oscillator.frequency.value = parseInt(touch0);
         //node.biQuad.frequency.value = parseInt(touch0)
         //node.oscillator.frequency.value = 100; // frequency in hertz
     };
@@ -80,7 +127,7 @@
         this.context = context;
         this.urlList = urlList;
         this.onload = callback;
-        this.bufferList = new Array();
+        this.bufferList = [];
         this.loadCount = 0;
     }
 
@@ -109,18 +156,19 @@
                 console.error('decodeAudioData error', error);
                 }
             );
-        }
+        };
+
         request.onerror = function() {
             alert('BufferLoader: XHR error');
-        }
+        };
 
         request.send();
-    }
+    };
 
     BufferLoader.prototype.load = function() {
         for (var i = 0; i < this.urlList.length; ++i)
         this.loadBuffer(this.urlList[i], i);
-    }
+    };
 
     function finishedLoading(bufferList) {
         // Create two sources and play them both together.
@@ -134,19 +182,19 @@
         document.addEventListener('keypress', function(ev) {
             switch (ev.charCode) {
                 case 97:
-                    playSound(kick, context.currentTime+0.100)
+                    playSound(kick, context.currentTime+0.100);
                 break;
                 case 115:
-                    playSound(snare, context.currentTime+0.100)
+                    playSound(snare, context.currentTime+0.100);
                 break;
                 case 100:
-                    playSound(hihat, context.currentTime+0.100)
+                    playSound(hihat, context.currentTime+0.100);
                 break;
-            }           
-        })
+            }
+        });
 
-        document.getElementById('kick').addEventListener('touchstart', function() { playSound(kick, context.currentTime+0.100) }, false);
-        document.getElementById('snare').addEventListener('touchstart', function() { playSound(snare, context.currentTime+0.100) }, false);
+        document.getElementById('kick').addEventListener('touchstart', function() { playSound(kick, context.currentTime+0.100); }, false);
+        document.getElementById('snare').addEventListener('touchstart', function() { playSound(snare, context.currentTime+0.100); }, false);
         var startTime = context.currentTime + 0.100;
         var tempo = 100; // BPM (beats per minute)
         var eighthNoteTime = (60 / tempo) / 2;
@@ -176,6 +224,49 @@
         source.start(time);
     }
 
-    
+    var getDirection = function (ev, obj) {
+        var w = obj.offsetWidth,
+            h = obj.offsetHeight,
+            x = (ev.pageX - obj.offsetLeft - (w / 2) * (w > h ? (h / w) : 1)),
+            y = (ev.pageY - obj.offsetTop - (h / 2) * (h > w ? (w / h) : 1)),
+            d = Math.round( Math.atan2(y, x) / 1.57079633 + 5 ) % 4;
+      
+        return d;
+    };
+
+    var addClass = function ( ev, obj, state ) {
+        var direction = getDirection( ev, obj ),
+            class_suffix = "";
+        
+        obj.className = "";
+        
+        switch ( direction ) {
+            case 0 : class_suffix = '-top';    break;
+            case 1 : class_suffix = '-right';  break;
+            case 2 : class_suffix = '-bottom'; break;
+            case 3 : class_suffix = '-left';   break;
+        }
+        
+        obj.classList.add( state + class_suffix );
+    };
+
+    var mouseIn = function(ev) {
+        addClass( ev, this, 'in' );
+    };
+
+    var mouseOut = function(ev) {
+        addClass( ev, this, 'out' );
+    };
+
+    var parseNodes = function(nodes) {
+        // bind events
+        nodes.forEach(function (el) {
+            el.addEventListener('mouseover', mouseIn, false);
+            el.addEventListener('touchmove', mouseIn, false);
+
+            el.addEventListener('mouseout', mouseOut, false);
+            el.addEventListener('touchend', mouseOut, false);
+        });
+    };
 
 })(Meteor);
